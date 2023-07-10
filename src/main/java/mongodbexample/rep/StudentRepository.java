809c0dd3d9a792a7aa79d2a65cb4c11dc38eb56d
Mohit <mohit.srivastava0786@gmail.com>
2023-07-10 12:02:52 +0530

    feat: rest apis to perform crud operation

---
+++ src/main/java/mongodbexample/rep/StudentRepository.java
@@ -0,0 +1,9 @@
+package mongodbexample.rep;
+
+import mongodbexample.models.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StudentRepository extends MongoRepository<Student,Integer> {
+
+
+}
